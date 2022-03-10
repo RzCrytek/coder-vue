5cb2fd4df67e5f6dade8afb3d3a3dc0a2c571ff2
Ivan Falcon <ivan.falcon@whiz.pe>
2022-03-09 19:45:05 -0500

    add vue-cli to web-components

---
+++ vue-cli/web-components/src/components/TableList.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <h2 class="text-center">{{ title }}</h2>
+    <table class="table table-striped" :class="background">
+      <thead>
+        <tr>
+          <th>Nombre</th>
+          <th>Categoría</th>
+          <th>Año</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        <tr v-for="item in data" :key="item.id">
+          <td>{{ item.name }}</td>
+          <td>{{ item.category }}</td>
+          <td>{{ item.year }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TableList',
+  props: {
+    title: String,
+    data: Array,
+    background: String,
+  },
+};
+</script>
+
+<style scoped></style>
